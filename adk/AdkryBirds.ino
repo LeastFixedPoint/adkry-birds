6fbd5dabff31f080762bc521b20b44a4a29277d3
Taras Sheremeta <sheremetat@gmail.com>
2012-02-18 13:02:48 +0200

    Added arduino scetch template

---
+++ adk/AdkryBirds.ino
@@ -0,0 +1,16 @@
+/**
+Angry Birds ADK project
+for Global Android DevCon @Kyiv
+
+Author: Taras Sheremeta
+Date: 18.02.2012
+Licence: Apache License 2.0
+*/
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
