bd93a10106d3e899f8db18b69b9250e69c09e3d6
Taras Sheremeta <sheremetat@gmail.com>
2012-02-18 13:12:38 +0200

    Add new version of arduino scetch

---
+++ adk/AdkryBirds/AdkryBirds.ino
@@ -0,0 +1,26 @@
+/**
+Angry Birds ADK project
+for Global Android DevCon @Kyiv
+
+Author: Taras Sheremeta
+Date: 18.02.2012
+Licence: Apache License 2.0
+*/
+#include <Servo.h>
+
+#include <Max3421e.h>
+#include <Usb.h>
+#include <AndroidAccessory.h>
+
+#include <CapSense.h>
+
+
+int pushServo = 9;
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
